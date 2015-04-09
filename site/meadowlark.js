c59938beb2c6cd62e8b23ab69c42cc8bbb61f253
rodrigo <jrvldam@gmail.com>
2015-04-09 11:16:28 +0200

    Add Fortunes Cookies

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -2,6 +2,12 @@ var express = require('express');
 
 var app = express();
 
+var fortunes = ["Conquer your fears or they conquer you",
+				"Rivers need springs",
+				"Do not fear what you don't know",
+				"You will have a pleasant surprise",
+				"Whenever possible, keep it simple"];
+
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -17,7 +23,8 @@ app.get('/', function(req, res)
 
 app.get('/about', function(req, res)
 {
-	res.render('about');
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', {fortune: randomFortune});
 });
 
 app.use(function(req, res)
